f0f87a46348f6cc71af14383f71ebe413cbb2119
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-29 15:48:45 +0530

    creating admin controller  class

---
+++ src/main/java/com/pharmassist/controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.pharmassist.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+	private final AdminService adminService;
+	
+}
