41f2d3932f57c5f6682d501dd6d48c8335dd5f0d
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-29 15:31:21 +0530

    adding main class

---
+++ src/main/java/com/pharmassist/PharmassistApplication.java
@@ -0,0 +1,13 @@
+package com.pharmassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmassistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmassist/PharmassistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
