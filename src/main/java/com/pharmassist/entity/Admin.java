1224bbabccef665d752c5f7a46e33ee20cf9d34e
busappagarisushma <sushmabusappagari@gmail.com>
2024-11-29 15:45:25 +0530

    creating admin entity class

---
+++ src/main/java/com/pharmassist/entity/Admin.java
@@ -0,0 +1,43 @@
+package com.pharmassist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Admin {
+
+	@Id
+	private String adminId;
+	private String email;
+	private String phoneNumber;
+	private String password;
+	
+	
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
