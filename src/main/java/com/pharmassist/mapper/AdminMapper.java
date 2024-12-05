e9fdbf7e5da75e138533af351464c622e0bb770b
busappagarisushma <sushmabusappagari@gmail.com>
2024-12-05 14:56:05 +0530

    creating adminmapper

--- src/main/java/com/pharmassist/mapper/AdminMapper.java
+++ src/main/java/com/pharmassist/mapper/AdminMapper.java
@@ -4,12 +4,24 @@ import org.springframework.stereotype.Component;
 
 import com.pharmassist.entity.Admin;
 import com.pharmassist.requestdto.AdminRequest;
+import com.pharmassist.responsedto.AdminResponse;
 
 @Component
 public class AdminMapper {
 
-	public Admin mapToAdmin(AdminRequest adminRequest) {
-		return null;
-		
+	public Admin mapToAdmin(AdminRequest adminRequest,Admin admin) {
+		admin.setEmail(adminRequest.getEmail());
+		admin.setPassword(adminRequest.getPassword());
+		admin.setPhoneNumber(adminRequest.getPhoneNumber());
+
+		return admin;
+	}
+
+	public AdminResponse mapToAdminResponse(Admin admin) {
+		AdminResponse adminResponse = new AdminResponse();
+		adminResponse.setAdminId(admin.getAdminId());
+		adminResponse.setEmail(admin.getEmail());
+
+		return adminResponse;
 	}
 }
